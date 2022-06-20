668ee2d6a01a71a0f2ad2280d3008bc123788645
sorrrb <cdrukepike@gmail.com>
2022-06-20 16:05:54 -0400

    Complete basic event logic for cursor tracking

--- javascript.js
+++ javascript.js
@@ -1,21 +1,35 @@
 let display = () => {
-  const STATIC_COUNT = 1;
   const GRID_DIMENSION = 16;
   const containerRef = document.querySelector('div.container');
   containerRef.classList.add('grid-template');
+
+  // General Helper Functions
+
+  function createSq(cntX, cntY) {
+    return `grid-area: ${cntY} / ${cntX} / ${cntY + 1} / ${cntX + 1};`;
+  }
+
+  // Event Listener Helper Functions
+
+  function styleBackground(e) {
+    e.target.classList.add('hover');
+  }
+
+  // Function to generate Grid
 
   let defineGrid = () => {
     for (let i = 1; i <= GRID_DIMENSION; i++) {
       for (let j = 1; j <= GRID_DIMENSION; j++) {
         const cell = document.createElement('div');
         cell.style.cssText = createSq(j,i);
+        cell.classList.add(`cell`);
         containerRef.appendChild(cell);
       }
     }
-  }
-
-  function createSq(cntX, cntY) {
-    return `grid-area: ${cntY} / ${cntX} / ${cntY + 1} / ${cntX + 1};`
+    const squares = document.querySelectorAll('div.cell');
+    squares.forEach((cell) => {
+      cell.addEventListener('mouseenter', styleBackground);
+    });
   }
 
   defineGrid();
